2537c5642638992977b6480d02b358b0a096e345
Dzikri Muhammad <notfnd123@gmail.com>
2022-01-29 08:43:05 +0700

    final changed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
@@ -21,6 +22,13 @@ const item2 = new Item({
 const item3 = new Item({
     name: "<-- Hit this to delete an item"
 });
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -52,11 +60,25 @@ app.get("/", (req, res) => {
     })
 })
 
-app.get("/work", (req, res) => {
-    res.render("list", {
-        title: "Work List",
-        listItem: workItems
-    });
+app.get("/:customListName", (req, res) => {
+    const topic = _.capitalize(req.params.customListName);
+    List.findOne({name: topic}, (err, result) => {
+        if(!err){
+            if(!result){
+                const list = new List({
+                    name: topic,
+                    items: [item1, item2, item3]
+                });
+                list.save();
+                res.redirect(`/${topic}`);
+            }else{
+                res.render("list", {
+                    title: result.name,
+                    listItem: result.items
+                })
+            }
+        }
+    })
 })
 
 app.get("/about", (req, res) => {
@@ -65,23 +87,48 @@ app.get("/about", (req, res) => {
 
 app.post("/", (req, res) => {
     const itemName = req.body.newItem;
+    const listItem = req.body.button;
+
     const item = new Item({
         name: itemName
     });
     
-    item.save();
-    res.redirect("/");
+    if(listItem === "Today"){
+        item.save();
+        res.redirect("/");
+    }else{
+        List.findOne({name: listItem}, (err, result) => {
+            if(!err){
+                result.items.push(item);
+                result.save();
+                res.redirect(`/${listItem}`);
+            }
+        })
+    }
 })
 
 app.post("/delete", (req, res) => {
-    const getItemId = req.body.check;
-    Item.findByIdAndRemove(getItemId, err => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.redirect("/");
-        }
-    })
+    const itemId = req.body.check;
+    const listName = req.body.listName;
+
+    if(listName === "Today"){
+        Item.findByIdAndRemove(itemId, err => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/");
+            }
+        });
+    }else{
+        List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: itemId}}}, (err, result) => {
+            if(!err){
+                res.redirect(`/${listName}`);
+            }else{
+                console.log(err);
+            }
+        });
+    }
+
 })
 
 app.listen("4000", () => {
